91c3ccd56828bc95841c4b21d512ce6e7eb1fdec
JohnWjck <neo_soti@hotmail.com>
2021-07-30 17:42:20 -0400

    v3

--- JuegoLogico/Assets/Scripts/Animacion/JohnMovement.cs
+++ JuegoLogico/Assets/Scripts/Animacion/JohnMovement.cs
@@ -6,7 +6,7 @@ public class JohnMovement : MonoBehaviour
 {
     public float Speed;
     public float JumpForce;
-    public GameObject BulletPrefab;
+    //public GameObject BulletPrefab;
 
     private Rigidbody2D Rigidbody2D;
     private Animator Animator;
@@ -29,10 +29,10 @@ public class JohnMovement : MonoBehaviour
         if (Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
         else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
 
-       // Animator.SetBool("running", Horizontal != 0.0f);
+       Animator.SetBool("running", Horizontal != 0.0f);
 
-        // Detectar Suelo
-        // Debug.DrawRay(transform.position, Vector3.down * 0.1f, Color.red);
+         //Detectar Suelo
+         Debug.DrawRay(transform.position, Vector3.down * 0.1f, Color.red);
         if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
         {
             Grounded = true;
